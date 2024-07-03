c1bf160fbcf6596483550a54c99186b98cb13eef
Jorge Lameiras <66096179+jorgelameiras@users.noreply.github.com>
2024-07-03 18:14:24 -0400

    Update BattleshipGame.java

--- BattleShip/BattleshipGame.java
+++ BattleShip/BattleshipGame.java
@@ -1,20 +1,30 @@
 // Michael Gallagher, Jorge Lameiras, and Zach Taylor
 // The project is the creation of the game Battleship
 
+import java.awt.Color;
+import java.awt.DisplayMode;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.util.Scanner;
+import java.awt.*;
+import javax.swing.JFrame;
 
-public class BattleshipGame {
+public class BattleshipGame extends JFrame{
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
+		
+		DisplayMode dm = new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
+		BattleshipGame bg = new BattleshipGame();
+		bg.run(dm);
+		
 		// Create players
-		Player player1 = new Player("Player 1", 8); // 8x8 board for player 1
-		Player player2 = new Player("Player 2", 8); // 8x8 board for player 2
+		Player player1 = new Player("Bot", 8); // 8x8 board for player 1
+		Player player2 = new Player("You", 8); // 8x8 board for player 2
 
-		// Place ships for each player
+		// Place ships bot player
 		player1.getBoard().placeShips();
-		player2.getBoard().placeShips();
-
+		//player2.getBoard().placeShips();
+		
 		// Game loop
 		Player currentPlayer = player1;
 		Player opponentPlayer = player2;
@@ -60,5 +70,26 @@ public class BattleshipGame {
 	private static boolean isValidMove(int row, int col, Board board) {
 		int boardSize = board.getSize();
 		return row >= 0 && row < boardSize && col >= 0 && col < boardSize;
+	}
+	
+	public void run(DisplayMode dm) {
+		setBackground(Color.PINK);
+		setForeground(Color.WHITE);
+		setFont(new Font("Arial", Font.PLAIN, 24));
+		
+		Screen s = new Screen();
+		
+		try {
+			s.setFullScreen(dm, this);	// "this" always references to the object you are working on. In this case is "s"
+			try {
+				Thread.sleep(5000);		// This make the screen do nothing for 5s once its in full screen mode
+			}catch(Exception ex){}
+		}finally {
+			s.restoreScreen();
+		}
+	}
+	
+	public void paint(Graphics g) {
+		g.drawString("This is gonnabe awesome", 200, 200);
 	}
 }
